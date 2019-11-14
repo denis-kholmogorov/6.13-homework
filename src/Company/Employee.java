31fc758607ca9a7a68909a9cf75d1c0e97e7c643
denis-kholmogorov <kholmogorov.denis@gmail.com>
2019-11-14 11:17:19 +0300

    1.2 version

--- src/Company/Employee.java
+++ src/Company/Employee.java
@@ -2,6 +2,6 @@ package Company;
 
 public interface Employee{
 
-    public int getMonthSalary(); // public не стал убирать для возможноного использования вне пакета
+    public int getMonthSalary(); // public не стал убирать для возможноного использования вне пакетаз
 
 }
